5779bd14b7dee448eb13cd88a6546f33f865e03c
MAD-BBBB <MAD-BBB@yandex.ru>
2016-06-08 11:21:07 +0400

    second

--- ajax.php
+++ ajax.php
@@ -2,9 +2,6 @@
 
 header('Content-type: application/json');
 
-$x = rand(1, 4);
-
-//if ($x % 2 == 1)
 if(isset($_POST['inputLastName']) && $_POST['inputLastName']=='Bull')
 {
     $response = array('valid' => false, 'message' => 'Somthing other, please');
@@ -13,8 +10,6 @@ else
 {
     $response = array('valid' => true);
 }
-
-//$response = array('valid' => true);
 
 sleep(1);
 echo json_encode($response);
